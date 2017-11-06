afab5871286391956dd146462634ae50375cdb05
yangqiang0 <quentinyy@gmail.com>
2017-11-06 12:01:57 +0800

    add cxf crm file

---
+++ crm/src/main/java/cn/me/crm/CustomerDaoImpl.java
@@ -0,0 +1,65 @@
+package cn.me.crm;
+
+import cn.me.crm.domain.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.jws.WebService;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebService(endpointInterface = "cn.me.crm.ICustomerDao")
+public class CustomerDaoImpl implements ICustomerDao{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Override
+    public List<Customer> findCustomerNotAssociate() {
+        String sql = "select * from t_customer where decidedzone_id is NULL";
+        List<Customer> customerList = jdbcTemplate.query(sql, new RowMapper<Customer>() {
+
+            @Override
+            public Customer mapRow(ResultSet rs, int i) throws SQLException {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String station = rs.getString("station");
+                String telephone = rs.getString("telephone");
+                String address = rs.getString("address");
+                String decidedzone_id = rs.getString("decidedzone_id");
+                return new Customer(id, name, station, telephone, address, decidedzone_id);
+            }
+        });
+        return customerList;
+    }
+
+    @Override
+    public List<Customer> findCustomerAssociate(String decidedzzone_id) {
+        String sql = "select * from t_customer where decidedzone_id =?";
+        List<Customer> customerList = jdbcTemplate.query(sql, new RowMapper<Customer>() {
+
+            @Override
+            public Customer mapRow(ResultSet rs, int i) throws SQLException {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String station = rs.getString("station");
+                String telephone = rs.getString("telephone");
+                String address = rs.getString("address");
+                String decidedzone_id = rs.getString("decidedzone_id");
+                return new Customer(id, name, station, telephone, address, decidedzone_id);
+            }
+        },decidedzzone_id);
+        return customerList;
+    }
+
+    @Override
+    public void associate(String id, String[] customerIds) {
+        String sql = "update t_customer SET decidedzone_id = NULL WHERE decidedzone_id = ?";
+        jdbcTemplate.update(sql,id);
+        String sql2 = "update t_customer set decidedzone_id = ? where id = ?";
+        for (String cid :customerIds
+             ) {
+            jdbcTemplate.update(sql2,id,cid);
+        }
+    }
+}
---
+++ crm/src/main/java/cn/me/crm/ICustomerDao.java
@@ -0,0 +1,13 @@
+package cn.me.crm;
+
+import cn.me.crm.domain.Customer;
+
+import javax.jws.WebService;
+import java.util.List;
+
+@WebService
+public interface ICustomerDao {
+    List<Customer> findCustomerNotAssociate();
+    List<Customer> findCustomerAssociate(String decidedzzone_id);
+    void associate(String id, String[] customerIds);
+}
